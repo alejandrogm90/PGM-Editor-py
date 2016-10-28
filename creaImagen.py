f2c070f6222d06bfffb89d4d7258860ff9e4ba40
Alejandro Gomez <alejandrogomezmartin90@gmail.com>
2016-10-28 12:24:44 +0200

    Update creaImagen.py

--- creaImagen.py
+++ creaImagen.py
@@ -1,33 +1,29 @@
 #! /usr/bin/env python3
 
-"""
-Author: Alejandro Gomez
-"""
-
 import os, sys
 from clases.Imagen import Imagen
 
 if __name__ == '__main__':
     """
-    Funcion main
+    Funtcion main
     """
     if (len(sys.argv)==3):
        if (os.path.exists(sys.argv[2])):          
           img1 = Imagen.leerImagen(sys.argv[2])
-          if (sys.argv[1]=="-todo"):
+          if (sys.argv[1]=="-all"):
              img1.crearTodo()
-          elif (sys.argv[1]=="-inversa"):
+          elif (sys.argv[1]=="-inverse"):
              nom=Imagen.dameExtensionYNombre(img1.getNombre())
-             Imagen.inversa(img1).imprimirEnFichero(nom[0]+'_inversa.'+nom[1])
-          elif (sys.argv[1]=="-transpuesta"):
+             Imagen.inversa(img1).imprimirEnFichero(nom[0]+'_inverse.'+nom[1])
+          elif (sys.argv[1]=="-transposed"):
              nom=Imagen.dameExtensionYNombre(img1.getNombre())
-             Imagen.transponer(img1).imprimirEnFichero(nom[0]+'_transpuesta.'+nom[1])
+             Imagen.transponer(img1).imprimirEnFichero(nom[0]+'_transposed.'+nom[1])
           else:
-              print("El parametro '"+sys.argv[1]+"' no existe.")
+              print("The parameter '"+sys.argv[1]+"' do not exist.")
        else:
-           print("El fichero '"+sys.argv[2]+"' no existe.")
+           print("The file '"+sys.argv[2]+"' do not exist.")
     else:
-        print(sys.argv[0]+" -[ARGUMENTO] [NOMBREFICHERO]")
+        print(sys.argv[0]+" -[ARGUMENT] [FILE-NAME]")
 
 
 
